b06912dbc9638c3a23a397f833b892787665037c
shadman-a <shadman@outlook.com>
2020-07-22 10:21:26 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    const url = "http://localhost:3000/dogs"
+    const table = document.getElementById('table-body')
+    const edit = document.getElementById('edit')
+
+
+    function fetchDogs() {
+      fetch(url)
+      .then (resp => resp.json())  
+      .then (dogs => dogs.forEach(dog => renderDogs(dog)))
+    }
+
+    function renderDogs(dog) {
+        const row = document.createElement('tr')
+        row.dataset.id = dog.id
+        row.innerHTML = `
+        <td>${dog.name}</td> <td>${dog.breed}</td> <td>${dog.sex}</td> <td><button id='edit'>Edit</button></td>
+        `
+        table.append(row)
+    }
+
+    function patchDog(id) {
+        
+    }
+    
+    fetchDogs()
+
+    //listners
+
+    document.addEventListener("click", function(e){
+        e.preventDefault
+        e.target.matches(edit)
+        patchDog(e.target.dataset.id)
+        console.log(e.target.dataset.id)
+
+    })
+
+
 })
