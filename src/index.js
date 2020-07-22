06ac21f54363f5054b7b5e67b7417a9ad34c97aa
DCR430 <daniel.c.reyes30@gmail.com>
2020-07-22 11:17:43 -0400

    final

--- src/index.js
+++ src/index.js
@@ -1,3 +1,70 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+const table = document.getElementById("table-body")
+// const button = document.getElementsByTagName("button")
+// console.log(button)
+
+
+    function fetchDogs(){
+        fetch("http://localhost:3000/dogs")
+        .then(resp => resp.json())
+        .then(dogs => dogs.forEach(dog => renderDog(dog)))
+    }
+
+
+
+    function renderDog(dog){
+        const dogTr = document.createElement("tr")
+        dogTr.dataset.id = dog.id
+        dogTr.innerHTML = `
+        <td>${dog.name}</td>
+        <td>${dog.breed}</td>
+        <td>${dog.sex}</td>
+        <td><button class=editbutton id=${dog.id}>Edit</button></td></tr>`
+        table.append(dogTr)
+   }
+
+   
+   
+
+        editbutton.addEventListener("click",function(e){
+
+
+
+
+
+
+
+     })
+
+
+    //  function patchDog(http://localhost:3000/dogs/'${id}', newDogobj){
+    //     fetch ({
+    //             method: "PATCH",
+    //             headers: {"Content-Type": "application/json",
+    //             Accept: "application/json"
+    //           },
+    //           body: JSON.stringify({
+    //             name: `${newDogobj.name}
+    //             breed: ${newDogobj.breed}
+    //             sex: ${newDogobj.sex}`
+    //           })
+    //           .then(resp => resp.json())
+    //           .then(newDogobj => renderDog(newDogobj))
+    //           })
+        
+
+
+
+   
+
+
+
+
+
+
+   fetchDogs()
+
+
+
 })
