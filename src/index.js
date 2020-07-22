334f240d608218685612a9d09f511039c06a4faa
dolly-d <dolly.desir@gmail.com>
2020-07-22 10:25:58 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const url = 'http://localhost:3000/dogs'  
+
+  function fetchDogs(){
+      fetch(url)
+      .then(resp => resp.json())
+      .then(dogs => dogs.forEach(dog => renderDogs(dog)))
+      
+    }
+
+    function renderDogs(dog){
+        const dogTable = document.getElementById('table-body')
+        const dogRow = document.createElement('tr')
+        dogRow.dataset.id = dog.id
+        dogRow.innerHTML = 
+        `<td>${dog.name}</td>
+        <td>${dog.breed}</td>
+        <td>${dog.sex}</td>
+        <td><button id='edit'>Edit Dog</button></td>
+        `
+        dogTable.append(dogRow)
+        
+    }
+
+    fetchDogs()
 
 })
