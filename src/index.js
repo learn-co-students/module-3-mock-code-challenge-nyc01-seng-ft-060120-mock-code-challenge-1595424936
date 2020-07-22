6c376658a7175cf2ec318f302f3d3da18983c9b5
travisprol0 <travisjohnprol@gmail.com>
2020-07-22 11:17:11 -0400

    everything except table auto updating

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
     
     */
    
-   const baseUrl = 'http://localhost:3000/dogs'
+   const baseUrl = 'http://localhost:3000/dogs/'
    const dogTable = document.getElementById('table-body')
    const dogForm = document.getElementById('dog-form')
     
@@ -31,19 +31,57 @@ document.addEventListener('DOMContentLoaded', () => {
             <td><button class="edit-button" id=${dog.id}>Edit Dog</button></td>
         </tr>
         `
-    }
-    
+    } 
     
     document.addEventListener('click', e => {
         if (e.target.className === "edit-button"){
-            console.log(e.target)
+            const dogId = e.target.id
+            fetch(baseUrl + e.target.id)
+            .then(res => res.json())
+            .then(dog => addDataToForm(dog, dogId))
         }
     })
-        // const editButton = document.getElementsByTagName('button')
-    const editButton = document.getElementById('edit-button')
-    console.log(editButton)
-    
-    
-    // buttonHandler()
+
+    function addDataToForm(dog, dogId){
+        dogForm.name.value = dog.name
+        dogForm.breed.value = dog.breed 
+        dogForm.sex.value = dog.sex
+        submitForm(dogId)
+    }
+
+    function submitForm(dogId) {
+        document.addEventListener("submit", e => {
+            e.preventDefault()
+            const name = dogForm.name.value
+            const breed = dogForm.breed.value
+            const sex = dogForm.sex.value
+            console.log(name)
+            console.log(breed)
+            console.log(sex)
+
+            const dogObj = {name, breed, sex}
+            
+            fetch((baseUrl + dogId), {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                },
+                body: JSON.stringify(dogObj)
+            })
+            .then(res => res.json())
+            .then(dog => console.log(dog))
+            .then(fetchDogsAgain())
+            
+        })
+    }
+
+    function fetchDogsAgain() {
+        dogTable.innerHTML = ""
+        fetch(baseUrl)
+        .then(res => res.json())
+        .then(dogs => getIndividualDogs(dogs))
+    }
+
     fetchDogs()
 })
