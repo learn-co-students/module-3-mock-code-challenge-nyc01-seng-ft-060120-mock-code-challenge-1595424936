26417a05530de729b21ae1983328498f0cbd70c5
scubaguba345 <brandon.gubitosa@gmail.com>
2020-07-22 11:14:38 -0400

    mental breakdown

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    const url = "http://localhost:3000/dogs"
+    const dogform = document.getElementById('dog-form')
+    
+   
+    
+//make fetch request to URL to get Dogs Data
+    function fetchDogs() {
+        fetch(url)
+        .then(response => response.json())
+        .then(dogs => {
+            renderDogs(dogs)
+        })
+    }
+
+    //render dogs to page
+    const renderDogs = (dogs) => {
+        dogs.forEach(dog => renderDogList(name, breed, sex))
+    }
+
+    function renderDogList(name, breed, sex) {
+    const tablerow = document.createElement('tr');
+        const dogName = document.createElement('td');
+        const dogBreed = document.createElement('td');
+        const dogSex = document.createElement('td');
+    }
+    
+ //
+   
+
+   
+
+    
+
+
+    fetchDogs();
+
 })
