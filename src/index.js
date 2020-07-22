a90cd72ead5006ad95584ab86b5ef9b2f63fd7f8
dr-dolce14 <dolcemascop1@gmail.com>
2020-07-22 10:56:31 -0400

    started on patch

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     const baseURL = "http://localhost:3000/dogs"
     const table = document.getElementById('table-body')
+    const form = document.getElementById('dog-form')
+    
+    // form.addEventListener('click', function(e) {
+    //     console.log(e.target)
+    // })
    
 
     function fetchDogs () {
@@ -21,9 +26,41 @@ document.addEventListener('DOMContentLoaded', () => {
         <td>${dog.name}</td> 
         <td>${dog.breed}</td> 
         <td>${dog.sex}</td> 
-        <td><button>Edit</button></td></tr>`
+        <td><button id=${dog.id} class="edit">Edit</button></td></tr>`
               
     }
+
+
+    document.addEventListener('click', function(e) {
+        if (e.target.className === "edit") {
+            const editButton = e.target
+            let id = editButton.id
+            
+            console.log(form)
+            let formData = {
+                name: form.name.value,
+                breed: form.breed.value,
+                sex: form.sex.value
+            }
+            
+            fetch(`${baseURL}/${id}`, {
+                method: "patch",
+                headers: {
+                    "content-type": "application/json",
+                    "accept": "application/json"
+                },
+                body: JSON.stringify (formData)
+            })
+            // .then?
+
+
+
+        } // first if statement
+    })
+    
+
+
+
 
     
 fetchDogs()
