071831ec7ca58690d561cbfe78ae67e230df4e9f
mstaso <mstaso1@gmail.com>
2020-07-22 10:12:23 -0400

    first deliverable complete and partial second

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    const baseUrl = 'http://localhost:3000/dogs'
+    const tableBody = document.getElementById('table-body')
+
+    editEventListener()
+
+    function fetchDogs(){
+        fetch(baseUrl)
+        .then(response => response.json())
+        .then(dogs => renderDog(dogs))
+    }
+
+    function renderDog(dogs){
+        dogs.forEach(dog =>{
+            const dogTr = document.createElement('tr')
+            const editButton = document.createElement('button')
+            editButton.innerText = 'Edit'
+            editButton.dataset.id = dog.id
+            dogTr.innerHTML = `<td>${dog.name}</td> <td>*${dog.breed}*</td> <td>*${dog.sex}*</td>` 
+            dogTr.append(editButton)
+            tableBody.append(dogTr)
+        })
+
+    }
+
+    function editEventListener(){
+        document.addEventListener('click', function(e){
+            if (e.target.innerText === 'Edit'){
+                console.log(e.target)
+            }
+        })
+    }
+
+    fetchDogs()
+
+
 })
