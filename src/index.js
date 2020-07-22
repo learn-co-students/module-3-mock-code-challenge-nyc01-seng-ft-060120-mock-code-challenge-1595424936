09abbb07abf652efa40febf8bd93321e532a905c
travisprol0 <travisjohnprol@gmail.com>
2020-07-22 09:58:37 -0400

    rendered data minus button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    /*
+        1. on page load, the dogs should be rendered
+            - get table element
+            - make fetch request
+
+    */
+
+    const baseUrl = 'http://localhost:3000/dogs'
+    const dogTable = document.getElementById('table-body')
+
+    function fetchDogs() {
+        fetch(baseUrl)
+        .then(res => res.json())
+        .then(dogs => getIndividualDogs(dogs))
+    }
+
+    function getIndividualDogs(dogs){
+        dogs.forEach(dog => renderDog(dog))
+    }
+
+    function renderDog(dog){
+        const row = dogTable.insertRow(0)
+        const nameCell = row.insertCell(0)
+        nameCell.innerText = dog.name
+        const breedCell = row.insertCell(1)
+        breedCell.innerText = dog.breed
+        const sexCell = row.insertCell(2)
+        sexCell.innerText = dog.sex
+        const editCell = row.insertCell(3)
+        console.log(dog)
+    }
+
+    fetchDogs()
 })
