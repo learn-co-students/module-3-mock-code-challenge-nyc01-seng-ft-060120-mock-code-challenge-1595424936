00ac438b3c6e6672024cfcd0d1c6d97c7455fe10
afrolambo <stephennoahgalvan@.com>
2020-07-22 11:17:46 -0400

    hard time with table

--- src/index.js
+++ src/index.js
@@ -1,3 +1,60 @@
-document.addEventListener('DOMContentLoaded', () => {
+const dogUrl = "http://localhost:3000/dogs"
+const registeredDogsList = document.getElementById('table-body')
+const dogTableRow = document.createElement('tr')
 
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  getDogs()
+      
 })
+
+function getDogs() {
+    fetch(dogUrl)
+    .then(response => response.json())
+    .then(dogs => renderAllDogs(dogs));
+  }
+
+  function renderAllDogs(dogs) {
+     dogs.forEach(dog => renderDog(dog) )
+  }
+
+
+  function renderDog(dog) {
+      const dogName = document.createElement('td')
+      const dogBreed = document.createElement('td')
+      const dogSex = document.createElement('td')
+      const editButton = document.createElement('td')
+
+      dogName.innerText = `${dog.name}` 
+      dogBreed.innerText = `${dog.breed}`
+      dogSex.innerText = `${dog.sex}`
+      editButton.innerHTML = document.createElement("button")
+
+      dogName.id = "name"
+      dogBreed.id = "breed"
+      dogSex.id = "sex"
+      editButton.id = "edit"
+
+      dogTableRow.append(dogName, dogBreed, dogSex)
+  }
+
+  function addRow() {
+    registeredDogsList.appendChild(dogTableRow)
+  }
+
+//upon load render dogs
+//fetch dogs 
+//render list of registered dogs 
+//dogs will be organized onto a table row 
+/* 
+<tr>
+    <td> Dog *Name </td>
+    <td>*Dog Breed</td> 
+    <td>*Dog Sex*</td> 
+    <td><button>Edit</button></td>
+</tr>
+
+
+*/
