ce199988bad83ca184ad343f986846b3be74cb55
alexpac27 <alexpac27@gmail.com>
2020-07-22 10:20:48 -0400

    working on form

--- src/index.js
+++ src/index.js
@@ -1,3 +1,64 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const dogUrl = "http://localhost:3000/dogs/"
+    const tableBody = document.querySelector('#table-body')
+    const inputName = document.querySelector('[name="name"]')
+    const inputBreed = document.querySelector('[name="breed"]')
+    const inputSex = document.querySelector('[name="sex"]')
+   
+
+
+    // console.log(tableBody)
+    
+
+
+    function fetchDogs(){
+        fetch(dogUrl)
+        .then( resp => resp.json() )
+        .then( dogs => renderAllDogs(dogs))
+    }
+
+    function renderAllDogs(dogs){
+        dogs.forEach(dog => renderDog(dog))
+    }
+
+    function renderDog(dog){
+        let tableRow = document.createElement('tr')
+        tableRow.innerHTML = `
+        <td>${dog.name}</td>
+        <td>${dog.breed}</td>
+        <td>${dog.sex}</td>
+        <td id="buttonTd"><button id="editButton" data-dog-id=${dog.id}>Edit</button></td>
+        `
+        
+        tableBody.append(tableRow)
+
+        tableRow.addEventListener('click', (e) =>{
+            if (e.target.id === "editButton"){
+                
+                inputName.value = dog.name
+                inputName.dataset.dogId = dog.id
+                inputBreed.value = dog.breed
+                inputSex.value = dog.sex
+            }
+        })
+
+    
+    }
+
+
+    function changeDogInfo(){
+        document.addEventListener('submit', (e) =>{
+            e.preventDefault()
+            console.log(e.target)
+            
+            // fetch()
+        })
+    }
+
+    
+    //INVOKE
+
+    fetchDogs();
+    changeDogInfo();
 
 })
