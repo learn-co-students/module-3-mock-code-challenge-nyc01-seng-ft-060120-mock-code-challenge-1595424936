3a783f9fd53db6e6b3f924d1d3042399ca041a6b
dr-dolce14 <dolcemascop1@gmail.com>
2020-07-22 10:27:09 -0400

    got the table populated finally

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const baseURL = "http://localhost:3000/dogs"
+    const table = document.getElementById('table-body')
+   
 
-})
+    function fetchDogs () {
+        fetch(baseURL)
+        .then(function(response) {
+            return response.json()
+        })
+        .then(function(json) {
+            json.forEach(dog => {
+                renderDog(dog)
+            })
+        })
+    }
+
+    function renderDog(dog) {
+        let dogRow = table.insertRow()
+        dogRow.innerHTML = `
+        <td>${dog.name}</td> 
+        <td>${dog.breed}</td> 
+        <td>${dog.sex}</td> 
+        <td><button>Edit</button></td></tr>`
+              
+    }
+
+    
+fetchDogs()
+}) // end of DOM content loaded
