f3765a1d41fc2a7ceaf9622ac714561ef8fdb3dc
travisprol0 <travisjohnprol@gmail.com>
2020-07-22 10:27:24 -0400

    click handler

--- src/index.js
+++ src/index.js
@@ -1,36 +1,49 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+    
     /*
-        1. on page load, the dogs should be rendered
-            - get table element
-            - make fetch request
-
+    1. on page load, the dogs should be rendered
+    - get table element
+    - make fetch request
+    
     */
-
-    const baseUrl = 'http://localhost:3000/dogs'
-    const dogTable = document.getElementById('table-body')
-
+   
+   const baseUrl = 'http://localhost:3000/dogs'
+   const dogTable = document.getElementById('table-body')
+   const dogForm = document.getElementById('dog-form')
+    
     function fetchDogs() {
         fetch(baseUrl)
         .then(res => res.json())
         .then(dogs => getIndividualDogs(dogs))
     }
-
+    
     function getIndividualDogs(dogs){
         dogs.forEach(dog => renderDog(dog))
     }
-
+    
     function renderDog(dog){
-        const row = dogTable.insertRow(0)
-        const nameCell = row.insertCell(0)
-        nameCell.innerText = dog.name
-        const breedCell = row.insertCell(1)
-        breedCell.innerText = dog.breed
-        const sexCell = row.insertCell(2)
-        sexCell.innerText = dog.sex
-        const editCell = row.insertCell(3)
-        console.log(dog)
+        
+        dogTable.innerHTML += `
+        <tr>
+            <td>${dog.name}</td> 
+            <td>${dog.breed}</td> 
+            <td>${dog.sex}</td> 
+            <td><button class="edit-button" id=${dog.id}>Edit Dog</button></td>
+        </tr>
+        `
     }
-
+    
+    
+    document.addEventListener('click', e => {
+        if (e.target.className === "edit-button"){
+            console.log(e.target)
+        }
+    })
+        // const editButton = document.getElementsByTagName('button')
+    const editButton = document.getElementById('edit-button')
+    console.log(editButton)
+    
+    
+    // buttonHandler()
     fetchDogs()
 })
